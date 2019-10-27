d025208cb1f8c572d8a49d154891338200013d54
avi <avi747av@gmail.com>
2019-10-27 11:26:44 +0200

    added user interest categories

--- raw_html/static/portal/javascript/filter.js
+++ raw_html/static/portal/javascript/filter.js
@@ -1,179 +1,113 @@
  let selectedValues=[];
 let selectedCategories=[];
 
-function filtercategory(target){
-    if (target === "all"){
+function filtercategory(target) {
+    if (target==="all") {
         $("ul.projects_list li").show(); //show all
-        // $("#selected_tags").empty();
         selectedCategories=''
     }
-    else {
-    //     let tags=$.makeArray($("#existing_tags").children()).map((item) =>item.id);
-    // let toBeShown=tags;
-    // for(let tag of selectedCategories){
-    //     toBeShown=toBeShown.filter((item) =>{
-    //         if(tag === item)
-    //         return item
-    //     });
-    // }
 
-    //    selectedCategories= $.makeArray(selectedCategories);
-       selectedCategories.push(target);
-       
-    //    selectedCategories=selectedCategories.toString();
-       $("ul.projects_list li").hide();
-       for(let tag of selectedCategories){
-$("."+tag).show();
-       }
-        // $("li").not(selectedCategories.toString()).hide();
-        // $("#selected_tags").append(`<li id=${target}>${event.target.outerText}</li>`);
-}
+    else {
+        selectedCategories.push(target);
+        $("ul.projects_list li").hide();
+
+        for(let tag of selectedCategories) {
+            $("."+tag).show();
+        }
+    }
 }
 
 function readURL(input) {
     if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      
-      reader.onload = function(e) {
-        $('#def_pic').attr('src', e.target.result);
-      }
-      
-      reader.readAsDataURL(input.files[0]);
+        var reader=new FileReader();
+
+        reader.onload=function(e) {
+            $('#def_pic').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
     }
-  }
+}
   
-  $("#id_img").change(function() {
+$("#id_img").change(function() {
     readURL(this);
-  });
-
-function filterTags(input){
-let found=false;
-let tagsContainer=$.makeArray($("#id_tags").children());
-tagsContainer=tagsContainer.map((tag) => tag.className);
-$("#id_tags").css("visibility","");
-if(event.keyCode===13 || input === ""){
-    $("#id_tags").toggle();
-    $("#id_tags").hide();
-    handleBtn();
-}
-else{
-// let val;
-        for (let category of tagsContainer){
-            val=category.value;
-                if(category.toUpperCase().indexOf(input.toUpperCase()) > -1){
-                    // $("option[value='" +val  + "']").show();
-                    $("."+category).show();
-                    found=true;
-            }
-                else{
-                    // $("option[value='" +val  + "']").hide();
-                    $("."+category).hide();
-                }
-        }
-        if(found){
-            $("#id_tags").show();
-            // $("#add_new_tag").hide();
-            handleBtn();
-        }
-        else{
-            $("#id_tags").hide();
-            // $("#add_new_tag").show();
-            handleBtn();
-        }
-        if(input !=='' && event.keyCode === 13 && $("#id_tags").is(":hidden"))
-            addNewTag(input);
-        }
-}
-function showAll(){
-    $("#id_tags").toggle().css("visibility","visible");
-    $("#id_tags").children().show();
-    handleBtn();
 }
 
-function handleBtn(){
-    if($("#id_tags").is(":visible")){
-        $("#show_tags_btn").text("Hide Tags");
-    }
-    else
-    $("#show_tags_btn").text("Show All Tags");
-}
-function handleSelection(val){
-    let tags=$.makeArray($("#id_tags").children()); /* selected=$.makeArray($("#id_tags").children("option:selected")) */;
-    /* selectedValues=Array.from(selectedValues);
-    selectedValues.push(selected[0].value);
-    selectedValues=selectedValues.toString(); */
-
-   /*  $.each(selectedValues.split(","), function(i,e){ //select multiple
-    $("#id_tags option[value='" + e + "']").prop("selected", true);
-    $("div.bootstrap-tagsinput input").val(e);
-    var element = $.Event( "keypress", { which: 13 } );
-$('div.bootstrap-tagsinput input').trigger(element);
-}); */
-    // let tag=tags[val - 1].value;
-    // $("#search_tag").val("");
-    // let children=$("#id_tags").children()
-    // $("#id_tags").css("visibility","hidden");
-    // $("#project_tags").append(`<li id=${val}>${tag}&nbsp; &#10006;</li>`);
-    // $("#"+ val).css({
-    //     "background-color": '#FBE555',
-    //     "fontSize": '15px',
-    //     'height': '25px',
-    //     'width':$("#"+val).outerWidth()+ 20 +'px'
-    // });
-    // $("#project_tags").unbind('click *');
-    // $("#project_tags").on('click','*',handleclick);
-    // var $input = $("input").tagsinput('input');
-// $input.addClass('custom-class');
-// $('input').tagsinput('add', 'vino');
-    // $('.custom-class').tagsinput('add',val);
-    // mytagsinput.tagsinput('add', { id: 1, text: 'mytext'});
-}
-
-function handleclick(event){
-    $("#"+event.target.id).remove();
-    if($("#project_tags").children().length == 1)
-    $("#project_tags").empty();
-    $("option[value='" +event.target.id  + "']").removeAttr('selected');
-}
-$("#add_new_tag").hide();
-
-function addNewTag(input){
-    let val=$("#id_tags").children().length +1;
-    $("#id_tags").append(`<li value=${val} selected>${input}</li>`);
-    $("#project_tags").unbind('click *');
-    handleSelection(val);
-}
+);
 
 $('#categories_form').on('keyup keypress', function(event) {
-    let keyCode = event.keyCode || event.which;
-    if (keyCode === 13) { 
-      event.preventDefault();
-      return false;
+    let keyCode=event.keyCode || event.which;
+
+    if (keyCode===13) {
+        event.preventDefault();
+        return false;
     }
-  });
-$("#id_tags").hide();
+}
 
-    $("[id$=search_tag]").tagsinput('items');
-$('input').keyup(function (event) {
-    filterTags(event.target.value);
-       });
+);
 
-$("#id_tags").children().on('click',function(){
-    handleSelection(event.target.parentNode.className)});
+let select_field=$("#select_tags");
+let search_field=$("#search_tag");
+let selected_tags=$("#selected_tags");
 
-    /* $("#show_tags_btn").keypress(function(e){
-        if(e.keyCode === 13){
-            e.preventDefault();
-        }
-     }); */
+select_field.tagsinput( {
+        tagClass: "edit-tag label label-info"
+    }
+);
 
-     /* $("#def_pic").mouseenter(function(){
-         $("#def_pic").css('opacity','0.3');
-         $(".edit").css("visibility","");
-         $(".middle").css("opacity",'1');
-     }).mouseleave(function(){
-         $("#def_pic").css('opacity','1');
-         $(".edit").css("visibility","hidden");
-         $(".middle").css("opacity",'0');
-     })
- */
+search_field.tagsinput({
+    tagClass: "search-tag label label-info"
+});
+
+let tags=$.makeArray($("#existing_tags").children());
+tags=tags.map((item)=>item.id);
+
+for (let tag of tags) {
+    select_field.tagsinput('add', tag);
+}
+
+select_field.on('itemRemoved', function(event) {
+        search_field.tagsinput('add', event.item);
+    }
+);
+
+select_field.on('itemAdded',function(){
+    $('.edit-tag').off('click',handleSelectField);
+    $('.edit-tag').on('click',handleSelectField);
+})
+
+search_field.on("itemAdded", function(event) {
+        selected_tags.append('<option selected="" value="'+ event.item + '">physics</option>');
+        $('.search-tag').off('click',handleSearchField);
+        $(".search-tag").on('click',handleSearchField);
+    }
+);
+
+search_field.on("itemRemoved", function(event) {
+        $("#selected_tags > option[value='"+event.item + "']").remove();
+        select_field.tagsinput('add', event.item);
+    }
+);
+
+/* $(".edit-tag").on('click',function(event){ 
+    $("#search_tag").tagsinput("add",event.target.innerText);
+    select_field.tagsinput("remove",event.target.innerText)
+}); */
+
+function handleSelectField(event){
+    select_field.tagsinput("remove",event.target.innerText);
+}
+
+function handleSearchField(event){
+        search_field.tagsinput('remove',event.target.innerText);
+    }
+$('.etid-tag').on('click',handleSelectField);
+
+
+
+let interstTags=$.makeArray($(".user-interest-tags").children());
+interstTags=interstTags.map((item) => item.id);
+$(".default")
+for(let item of interstTags){
+    $(".")
+}
