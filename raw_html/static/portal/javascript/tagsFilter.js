43b70516d2bd1d6537e44271fa10c9001df04842
avi <avi747av@gmail.com>
2019-10-28 10:03:58 +0200

    important changes! i split filter.js to 2 files. hompage.html has tagsFilter.js file. i added in hompage.html existing_tags and user-interest-tags. i changed drop 1 to categories container

--- raw_html/static/portal/javascript/tagsFilter.js
+++ raw_html/static/portal/javascript/tagsFilter.js
@@ -1,34 +1,57 @@
 let interestTags=$.makeArray($("#user-interest-tags").children()); //interestTags will contain all the categories the user interested in
-interestTags=interestTags.map((item) => {
-   let value = item.id
-value=value.replace(/_/g, ' ');
-return value});//
-for(let item of interestTags){//appending the user interested tags
-    $(".categories-container").append(`<button class="btn" onclick="filtercategory('${item}')">${item}</button>`);
+
+interestTags=interestTags.map((item)=> {
+        let value=item.id value=value.replace(/_/g, ' ');
+        return value
+    }
+
+); //
+
+for(let item of interestTags) {
+
+    //appending the user interested tags
+    $(".categories-container").append(`<button class="btn"onclick="filtercategory('${item}')">$ {
+            item
+        }
+
+        </button>`);
 }
 
 let children=$.makeArray($(".categories-container").children()).length; //how many categories already has in categories container
 let maxCategoriesLength=4;
-if (children<maxCategoriesLength){
-let moreTags=$.makeArray($("#existing_tags").children());//how many tags to add
-moreTags=moreTags.map((item) => {
-    let value = item.id
-   value= value.replace(/_/g, ' ');
-    return value});
-for (let i=0;i<interestTags.length;i++){//deleting tags that we already apended
-    let index =moreTags.indexOf(interestTags[i]);
-    if(index > -1)
-    moreTags.splice(index, 1);
-}
-let numberOfAdditions=Math.min(maxCategoriesLength-children,moreTags.length)
-for(let i=0;i<numberOfAdditions;i++){//adding more tags
-    $(".categories-container").append(`<button class="btn" onclick="filtercategory('${moreTags[i]}')">${moreTags[i]}</button>`)
-}
+
+if (children<maxCategoriesLength) {
+    let moreTags=$.makeArray($("#existing_tags").children()); //how many tags to add
+
+    moreTags=moreTags.map((item)=> {
+            let value=item.id value=value.replace(/_/g, ' ');
+            return value
+        }
+
+    );
+
+    for (let i=0; i<interestTags.length; i++) {
+        //deleting tags that we already apended
+        let index=moreTags.indexOf(interestTags[i]);
+        if(index > -1) moreTags.splice(index, 1);
+    }
+
+    let numberOfAdditions=Math.min(maxCategoriesLength-children, moreTags.length) for(let i=0; i<numberOfAdditions; i++) {
+
+        //adding more tags
+        $(".categories-container").append(`<button class="btn"onclick="filtercategory('${moreTags[i]}')">$ {
+                moreTags[i]
+            }
+
+            </button>`)
+    }
 }
 
-let selectedCategories=[];//will contain the filter history for the filterCategory function
+let selectedCategories=[]; //will contain the filter history for the filterCategory function
 
-function filtercategory(tag) {//filter project by category
+function filtercategory(tag) {
+
+    //filter project by category
     if (tag==="all") {
         $("ul.projects_list li").show(); //show all
         selectedCategories=''
@@ -44,6 +67,4 @@ function filtercategory(tag) {//filter project by category
     }
 }
 
-function filter(input){
-
-}
+function filter(input) {}
