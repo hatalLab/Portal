6f0a7c56dbe6a11bb9b3177c6713717258c424a4
avi <avi747av@gmail.com>
2020-01-08 23:51:02 +0200

    added modal, tags section and comments to home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,95 @@
-import React, {Component} from 'react'
+import React, { Component, useState } from 'react'
 import styled from 'styled-components'
 import ModalPage from '../Components/Project_List/Modalpage'
 import { data as projectsData, tags, favoriteTags as FavoriteTags, interestedProjects } from '../static/data/rowData'
 import Comment from '../Components/Comments'
 
 
- // styled components
+// styled components
+// main container
+const HomeContainer =styled.div`
+direction: rtl;
+`
+// container with flex-direction of row
+const StyledRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  dir:rtl;
+  padding: 10px 40px;
+`
+// container of all categories
+const StyledBar = styled(StyledRow)`
+    background-color: #dce9ed;
+    padding: 0;
+`
+// container of every category
+const StyledTagContainer = styled.div`
+    margin: 0;
+    padding: 20px;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    &:hover {
+        background-color: black;
+        & > * {
+            color: white;
+        }
+    }
+`
+// the categories
+const StyledTag = styled.a`
+    padding: 0px 10px;
+    color: black;
+    &:hover {
+        text-decoration: none;
+    }
+`
+// the search project component 
+const StyledInput =styled.input`
+    size:"35";
+`
+/*Background and Modal open is used for modal that containing therest of the categories*/
 
+// Background is outside of the modal
+const Background =styled.div`
+    z-index: 3;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    opacity:    ${props => props.open ? "1" : "0"};
+    visibility: ${props => props.open ? "visible" : "hidden"};
+`
+
+// ModalOpen is the modal
+const ModalOpen = styled.div`
+    z-index: 4;
+    position: fixed;
+    transition: -webkit-transform 0.3s ease-out 0s;
+    will-change: transform;
+    overflow-y: auto;
+    background: #dce9ed;
+    width: 80%;
+    left: 50%;
+    text-align: center;
+    height: 40vh;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    border-radius: 10px 10px 0 0;
+    right: 0px;
+    transform: ${props => props.open ? "translateX(-50%)" : "translateY(-100%)" };
+    visibility: ${props => props.open ? "visible" : "hidden"};
+    box-shadow:rgba(0, 0, 0 ,0.15) -2px 2px 4px;
+`
+// container of the project list
 const StyledContainer = styled.div`
     display: flex;
     width: 90%;
     flex-wrap: wrap;
     margin: 100px auto;
 `;
-
+// ul of the project list
 const StyledList=styled.ul`
     display: flex;
     flex-wrap: wrap;
@@ -25,49 +101,95 @@ const StyledList=styled.ul`
         padding: 10px 5px;
         text-align: center;
     }
-`;
-
-const StyledFilterContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    margin: 50px auto;
-    width: 60vw;
-`;
-
-const StyledTagsContainer = styled.div`
-    display: flex;
-    flex-direction: row;
 `
 
-const StyledTag = styled.a`
-    padding: 20px 10px;
+// const StyledFilterContainer = styled.div`
+//     display: flex;
+//     justify-content: center;
+//     flex-direction: column;
+//     margin: 50px auto;
+//     width: 60vw;
+// `;
 
-`
-const StyledRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-direction: row;
-  dir:rtl;
-  padding:10px 40px;
-`;
 
-const StyledCenteredRow=styled(StyledRow)`
-    justify-content: center;
-`
-const StyledCol = styled.div`
-display: flex;
-flex-direction: column;
-dir:rtl;
-padding:10px;
-& > input {
-    width: 100px;
+// const StyledTagsContainer = styled.div`
+//     display: flex;
+//     flex-direction: row;
+// `
+
+
+
+
+
+// const StyledCenteredRow=styled(StyledRow)`
+//     justify-content: center;
+//`
+// const StyledCol = styled.div`
+// display: flex;
+// flex-direction: column;
+// dir:rtl;
+// padding:10px;
+// `;
+
+
+
+
+
+
+// determine how many tags to show in the tags section
+let numberOfTags=5;
+
+// handling tags section. handeling tags section content, opening and closing modal of rest of tags and its content 
+const Modal = (props) => {
+    // state for should modal be open or not
+    const [modalShow, setModalShow] = useState(false);
+    let tagsList = [], index, restTags =[...tags]
+    // insert interested tags first to tags section and remove them from restTags
+    for (let tag of FavoriteTags){
+        tagsList.push(<StyledTagContainer  key={tag} className = "container"><StyledTag onClick ={event => props.handleChange(event)} name = {tag} href="#">{tag}</StyledTag></StyledTagContainer>)
+        index=restTags.indexOf(tag)
+        restTags.splice(index,1)
+    }
+    // calculate how many tags to add to tags section and the rest will be shwon in the modal
+    let TagsToAdd = Math.abs(numberOfTags - tagsList.length)
+    // insert the rest of the tags
+    for(let i =0;i<TagsToAdd;i++){
+        tagsList.push(<StyledTagContainer  key={restTags[i]} className = "container"><StyledTag onClick ={event => props.handleChange(event)} name = {restTags[i]} href="#">{restTags[i]}</StyledTag></StyledTagContainer>)
+    }
+    // remove from rest tags the tags that we added to tags section in the last loop
+    restTags.splice(0,TagsToAdd)
+    tagsList.push(<StyledTagContainer key = "ModalOpen" onClick = {() => setModalShow(true)}> &#11167;<StyledTag></StyledTag></StyledTagContainer>)
+  return (
+      <>
+        <StyledBar>
+                {tagsList}
+        </StyledBar>
+        <Background onClick = {() => setModalShow(false)} open = {modalShow} />
+            <div className="xyz">
+        <ModalOpen open ={modalShow}>
+            <ModalContent AllTags={restTags} handleChange={props.handleChange} />
+        </ModalOpen>
+            </div>
+      </>
+  )
 }
-`;
 
-const HomeContainer =styled.div`
-direction: rtl;
-`
+// this function is responsible for the content of the modal (insert the rest of the tags)
+const ModalContent = (props) => {
+    let list =[]
+    list.push(<StyledTagContainer key="XXX" onClick = {event => props.handleChange(event)}><p name = "xxx">ב</p></StyledTagContainer>)
+    for(let tag of props.AllTags){
+        list.push(<StyledTagContainer key = {tag}><StyledTag onClick = {event => props.handleChange(event)}>{tag}</StyledTag></StyledTagContainer>)
+    }
+    return (
+        <div>
+        {list}
+        </div>
+    )
+}
+
+
+
 class ControlledHomePage extends Component{  
     constructor(){
         super()
@@ -153,7 +275,7 @@ class ControlledHomePage extends Component{
     // handle change in the input field
     handleChange (event){
         event.preventDefault();
-        const {name, value, type} = event.target
+        const {name, value, type, innerText} = event.target
         if(type){
             this.setState({
                 input:value
@@ -192,16 +314,16 @@ class ControlledHomePage extends Component{
     render(){
         return(
             <HomeContainer>
-            <StyledFilterContainer>
+            {/* <StyledBar> */}
+            <Modal handleChange = {this.handleChange} />
+                        
+                        {/* <TagsContainer handleChange = {this.handleChange} /> */}
+                    {/* </StyledBar> */}
+                            <StyledInput name = "search" placeholder = " חפש קטגוריה" value = {this.state.input} onChange = {this.handleChange} />
+            {/* <StyledFilterContainer>
                 <StyledCol>
-                        <StyledCenteredRow>
-                            <input name = "search" placeholder = "search category" value = {this.state.input} onChange = {this.handleChange} />
-                        </StyledCenteredRow>
-                    <StyledRow>
-                        <FilterSection handleChange = {this.handleChange} />
-                    </StyledRow>
                 </StyledCol>
-            </StyledFilterContainer>
+            </StyledFilterContainer> */}
             <StyledContainer id ="project_list" > {/*styled component */}
                 <Comment>start of project list</Comment>
                 <StyledList id="mix_wrapper"> {/*styled component */}
@@ -221,18 +343,6 @@ const sortProjects = (obj) => {
 }
 
 
-const FilterSection = (props)=> {
-    let tagsList = []
-    for (let tag of tags){
-        tagsList.push(<StyledTag onClick ={event => props.handleChange(event)} name = {tag} key={tag} href="#">{tag}</StyledTag>)
-    }
-
-    return (
-        <>
-            {tagsList}
-        </>
-    )
-}
 
 
 export default ControlledHomePage
