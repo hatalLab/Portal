bea7bd1b1ab5265583b55526e0c6ff188acc9547
a740david <54502632+a740david@users.noreply.github.com>
2020-02-03 17:13:01 +0200

    add 3

--- src/App.js
+++ src/App.js
@@ -2,46 +2,42 @@ import React from 'react'
 import { Label,Button } from 'semantic-ui-react'
 
 import './App.css'
-const arr =["programming","c","c++","java","c#","java_script","react","react_native"]
+const arr =["programming","c","c++","java","c#","react","react native","java script"]
 const arr1=[
 "html","d3","php","ruby",
 "swift","jquery","sql","python","unity","matlab",
-"raspberry_ip","typescript","data_script","flask",
-"r","vue","arduino","net","computer_vision","chemistry",
-"augmented_reality","artificial_intlligence","engineering",
-"electronics","electro_optic","electric","drone","physics",
+"raspberry ip","type script","data script","flask",
+"r","vue","arduino","net","computer vision","chemistry",
+"augmented reality","artificial intlligence","engineering",
+"electronics","electro optic","electric","drone","physics",
 "optic","mechanics","mathematics","symfony","robtics"]
-
-
 function Wrapper(){
   let list =[]
-let i = 0
   for(let tag of arr){
     list.push(
       <Label as='a' image> 
-  <Button key={i} onClick={(e) => console.log(e.target)} >
+  <Button  onClick={(e) => console.log(`${tag}`)} >
     <img src={`${tag}.png`} alt={`${tag}`} />
        {tag}
        </Button>
         </Label>  
     )
-    i++
+   
   }
   return  list
 }
 function Wrapper1(){
- let i = 0;
+
   let list1=[]
   for(let tag of arr1){
     list1.push(
       <Label as='a' image> 
-     <Button key={i} onClick={(e) => console.log(e.target)} >
+     <Button onClick={(e) => console.log(`${tag}`)} >
       <img src={`${tag}.png`} alt={`${tag}`} />
        {tag}
        </Button>
         </Label>  
     )
-    i++
   }
   return list1
 }
@@ -52,15 +48,10 @@ function App(){
   return (   
 <div>
   <div>
- 
     {list2}
-   
     </div>
-    {list3}
-    
+    {list3}  
   </div>
   )
 }
-
-
 export default App;
