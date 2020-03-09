c160289245674b7e0fd3f8ca04b2e8d496d79ed7
avi747av <avi747av@gmail.com>
2020-03-09 17:05:30 +0200

    deleted unused files

--- src/App.js
+++
@@ -1,9 +0,0 @@
-/* import React from 'react'
-import {Link, Switch, Route} from 'react-router-dom'
-
-function App(){
-return(
-
-)
-
-} */
--- src/Components/Project_List/try.js
+++
@@ -1,4 +0,0 @@
-// const fs =require('fs')
-
-// const data =fs.readFileSync('../../static/data/DataBase.json')
-// console.log(data.toString());
